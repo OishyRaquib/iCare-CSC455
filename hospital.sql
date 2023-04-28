13228e9071f48201f3e8775ff9dbe284f11522ba
Foyzul Hoque <foyzul.aman@gmail.com>
2023-04-28 23:03:23 +0600

    database

---
+++ hospital.sql
@@ -0,0 +1,84 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 28, 2023 at 07:00 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hospital`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `department`
+--
+
+CREATE TABLE `department` (
+  `dep_name` varchar(50) NOT NULL,
+  `dep_des` varchar(500) NOT NULL,
+  `dep_rev` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctor_details`
+--
+
+CREATE TABLE `doctor_details` (
+  `doc_name` varchar(50) NOT NULL,
+  `doc_phone` varchar(50) NOT NULL,
+  `doc_exp` varchar(50) NOT NULL,
+  `doc_dept` varchar(50) NOT NULL,
+  `doc_pass` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patient_details`
+--
+
+CREATE TABLE `patient_details` (
+  `pat_name` varchar(50) NOT NULL,
+  `pat_age` int(11) NOT NULL,
+  `pat_phone` varchar(50) NOT NULL,
+  `pat_pass` varchar(50) NOT NULL,
+  `pat_gender` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `report`
+--
+
+CREATE TABLE `report` (
+  `rep_pname` varchar(50) NOT NULL,
+  `rep_dname` varchar(50) NOT NULL,
+  `rep_pphone` varchar(50) NOT NULL,
+  `rep_dphone` varchar(50) NOT NULL,
+  `rep_day` varchar(50) NOT NULL,
+  `rep_treatdetails` varchar(500) NOT NULL,
+  `rep_med` varchar(500) NOT NULL,
+  `rep_specialins` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
